379e93ed5b60e7910454a00d2e5ed21db1fe9cc6
Roswell Cua <ross.cua@gmail.com>
2021-06-04 02:48:03 -0700

    changed fib file

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -5,7 +5,7 @@ class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
-    index: '',
+    index: ''
   };
 
   componentDidMount() {
@@ -21,18 +21,9 @@ class Fib extends Component {
   async fetchIndexes() {
     const seenIndexes = await axios.get('/api/values/all');
     this.setState({
-      seenIndexes: seenIndexes.data,
+      seenIndexes: seenIndexes.data
     });
   }
-
-  handleSubmit = async (event) => {
-    event.preventDefault();
-
-    await axios.post('/api/values', {
-      index: this.state.index,
-    });
-    this.setState({ index: '' });
-  };
 
   renderSeenIndexes() {
     return this.state.seenIndexes.map(({ number }) => number).join(', ');
@@ -48,9 +39,17 @@ class Fib extends Component {
         </div>
       );
     }
-
     return entries;
   }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    await axios.post('/api/values', {
+      index: this.state.index
+    });
+    this.setState({ index: '' });
+  };
 
   render() {
     return (
@@ -59,14 +58,13 @@ class Fib extends Component {
           <label>Enter your index:</label>
           <input
             value={this.state.index}
-            onChange={(event) => this.setState({ index: event.target.value })}
+            onChange={event => this.setState({ index: event.target.value })}
           />
           <button>Submit</button>
         </form>
 
-        <h3>Indexes I have seen:</h3>
+        <h3>Indexes I have seen</h3>
         {this.renderSeenIndexes()}
-
         <h3>Calculated Values:</h3>
         {this.renderValues()}
       </div>
